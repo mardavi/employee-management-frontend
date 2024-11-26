cbc4b96ed6c6d1cf4bec642ef12d5f70a06996ae
JustineA523 <jaldea28@gmail.com>
2024-11-26 17:44:35 -0500

    Employee Folder added
    Added a employee folder

---
+++ src/employees/AddEmployee.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const AddEmployee = () => {
+    const [employee, setEmployee] = useState ({
+        name:'',
+        position:'',
+        email:'',
+        phone: '',
+        department
+    })
+}
