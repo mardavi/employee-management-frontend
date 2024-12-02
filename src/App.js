21168ea6a351cfb151bd2a136421ff34498c6187
mardavi <dmar@my.centennialcollege.ca>
2024-12-02 17:02:27 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React from "react";
-import './App.css';
-import logo from './logo.svg';
-import EmployeeList from './employeeList/EmployeeList';
-import ViewDeleteEmployee from "./employees/ViewDeleteEmployee";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import EmployeeList from "./employees/EmployeeList";
 import AddEmployee from "./employees/AddEmployee";
 import EditEmployee from "./employees/EditEmployee";
+import ViewDeleteEmployee from "./employees/ViewDeleteEmployee";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/employees" element={<EmployeeList />} />
+          <Route path="/add-employee" element={<AddEmployee />} />
+          <Route path="/edit-employee/:id" element={<EditEmployee />} />
+          <Route path="/view-delete-employee/:id" element={<ViewDeleteEmployee />} />
+        </Routes>
+      </main>
+    </Router>
   );
-}
+};
 
 export default App;
