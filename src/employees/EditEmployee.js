2565542d5a7be1df3f7cfe0b42cde9f8f908018c
JustineA523 <jaldea28@gmail.com>
2024-12-02 17:01:47 -0500

    Update EditEmployee.js

--- src/employees/EditEmployee.js
+++ src/employees/EditEmployee.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
 
 const EditEmployee = () =>{
+const { id } = useParams();
+
     const[employee, setEmployee] = useState({
         name: '',
         position: '',
